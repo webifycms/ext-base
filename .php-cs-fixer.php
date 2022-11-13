bdaafef24fe6521f84da18d91324b0e6371be0c8
Mohammed Shifreen <mshifreen@gmail.com>
2022-11-13 09:07:21 +0530

    .php-cs-fixer.php file has been added back to the version control.

---
+++ .php-cs-fixer.php
@@ -0,0 +1,18 @@
+<?php 
+
+// should require the composer autoloader on first
+require __DIR__ . '/vendor/autoload.php';
+
+use OneCMS\Tools\Fixer;
+use PhpCsFixer\Finder;
+
+$finder = Finder::create()
+    ->in([
+        __DIR__ . '/src',
+        __DIR__ . '/test',
+    ])
+    ->name('*.php');
+
+return (new Fixer($finder))
+    ->getConfig()
+    ->setUsingCache(false);
